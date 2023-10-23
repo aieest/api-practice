e96195a5044284cc6ecaae42ad9ba9063895b6ab
August Estidola <aie.estidola@gmail.com>
2023-10-23 09:28:57 +0800

    used random_users as main route

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root to: 'api/random_users#show'
+
+  namespace :api do
+    resource :random_users, only: :show
+  end
 end
